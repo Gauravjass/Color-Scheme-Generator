6f3094dfd739ab2e16b24dfbd477e8825b8a5de3
Gauravjass <jassalgaurav020@gmail.com>
2026-01-28 21:32:25 -0500

    Added functionality to copy color hex-code to clipboard

--- index.js
+++ index.js
@@ -1,58 +1,60 @@
-const getColorBtn = document.getElementById('get-color-btn')
-const colorPicker = document.getElementById('color-picker')
+const getColorBtn = document.getElementById("get-color-btn");
+const colorPicker = document.getElementById("color-picker");
+const container = document.getElementById("container");
+const theme = document.getElementById("theme");
 
-getColorBtn.addEventListener('click', function(){
-    let colorValue = colorPicker.value
-    colorValue = colorValue.substring(1)
-    console.log(colorValue)
-    selectedValue = theme.value
-    let url = ""
-    if(selectedValue == 'monochrome'){
-        url = `https://www.thecolorapi.com/scheme?hex=${colorValue}&format=json&mode=monochrome&count=6`
-    } 
-    else if(selectedValue == 'monochrome-dark'){
-        url = `https://www.thecolorapi.com/scheme?hex=${colorValue}&format=json&mode=monochrome-dark&count=6`
-    } 
-    else if(selectedValue == 'monochrome-light'){
-        url = `https://www.thecolorapi.com/scheme?hex=${colorValue}&format=json&mode=monochrome-light&count=6`
-    } 
-    else if(selectedValue == 'analogic'){
-        url = `https://www.thecolorapi.com/scheme?hex=${colorValue}&format=json&mode=analogic&count=6`
-    } 
-    else if(selectedValue == 'complement'){
-        url = `https://www.thecolorapi.com/scheme?hex=${colorValue}&format=json&mode=complement&count=6`
-    } 
-    else if(selectedValue == 'analogic-complement'){
-        url = `https://www.thecolorapi.com/scheme?hex=${colorValue}&format=json&mode=analogic-complement&count=6`
-    } 
-    else if(selectedValue == 'triad'){
-        url = `https://www.thecolorapi.com/scheme?hex=${colorValue}&format=json&mode=triad&count=6`
-    } 
-    else if(selectedValue == 'quad'){
-        url = `https://www.thecolorapi.com/scheme?hex=${colorValue}&format=json&mode=quad&count=6`
-    } 
-    
-    fetch(url)
-        .then(res => res.json())
-            .then(data => {
-                let html = "";
-                
-                for(color of data.colors){  
-                    html +=`<div class="box" style = "background-color:${color.hex.value}"><p class= "hex-code">${color.hex.value}</p></div>
-                    `;   
-                }
-                document.getElementById("container").innerHTML = html
-                })
-})
+// intital state of the app
+getColorScheme("F55A5A", "monochrome");
 
-fetch("https://www.thecolorapi.com/scheme?hex=F55A5A&format=json&mode=monochrome&count=6")
- .then(res => res.json())
-    .then(data => { 
-        let html = "";
-        
-        for(color of data.colors){    
-            html +=`<div class="box" style = "background-color:${color.hex.value}"><p class= "hex-code">${color.hex.value}</p></div>
-            `;  
-        }
-        document.getElementById("container").innerHTML = html
-    })
+getColorBtn.addEventListener("click", () => {
+  const colorValue = colorPicker.value.substring(1);
+  const selectedtheme = theme.value;
+  getColorScheme(colorValue, selectedtheme);
+});
+
+function getColorScheme(hex, mode) {
+  const url = `https://www.thecolorapi.com/scheme?hex=${hex}&format=json&mode=${mode}&count=5`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => renderColorScheme(data.colors))
+    .catch((err) => console.error(err));
+}
+
+function renderColorScheme(colors) {
+  container.innerHTML = colors
+    .map(
+      (color) =>
+        `<div class="box" style = "background-color:${color.hex.value}" data-color = ${color.hex.value}>
+                <span class="hex-code">${color.hex.value}</span>
+            </div>
+        `,
+    )
+    .join("");
+}
+
+container.addEventListener("click", async (event) => {
+  const box = event.target.closest(".box");
+  if (!box) return;
+
+  const color = box.dataset.color;
+
+  try {
+    await navigator.clipboard.writeText(color);
+    showToast("Color code successfully copied to clipboard");
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+// toast a message
+
+const toast = document.getElementById("toast");
+
+function showToast(message, duration = 2000) {
+  toast.textContent = message;
+  toast.classList.add("show");
+  setTimeout(() => {
+    toast.classList.remove("show");
+  }, duration);
+}
